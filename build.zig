08f74853b8edf5c5bd1ff8ecdb518d76085684be
Ethan Evans <ethanalexevans@gmail.com>
2024-01-12 22:05:15 -0800

    initial

--- build.zig
+++ build.zig
@@ -1,6 +1,28 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
+pub fn buildRaylib(b: *std.Build) *std.Build.Step {
+    const raylib_prebuild = b.addSystemCommand(&.{
+        "cmake",
+        "-S",
+        "deps/raylib",
+        "-B",
+        "deps/raylib/build",
+    });
+
+    const raylib_build = b.addSystemCommand(&.{
+        "cmake",
+        "--build",
+        "deps/raylib/build",
+        "--config",
+        "Release",
+        "-j",
+    });
+
+    raylib_build.step.dependOn(&raylib_prebuild.step);
+    return &raylib_build.step;
+}
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -13,18 +35,22 @@ pub fn build(b: *std.Build) void {
     }
 
     const exe = b.addExecutable(.{
-        .name = "hello",
+        .name = "raylib-quickstart",
         .target = target,
         .optimize = optimize,
     });
+
+    exe.addIncludePath(.{ .path = "include" });
+    exe.addIncludePath(.{ .path = "deps/raylib/build/raylib/include" });
 
     const c_files = .{
         "src/main.c",
     };
     const c_flags = .{
+        "-std=c99",
         "-Wall",
         "-Wextra",
-        "-Werror",
+        //"-Werror",
         "-pedantic",
     };
 
@@ -34,7 +60,13 @@ pub fn build(b: *std.Build) void {
     if (is_windows) {
         exe.linkSystemLibrary("user32");
         exe.linkSystemLibrary("shell32");
+        exe.linkSystemLibrary("gdi32");
+        exe.linkSystemLibrary("winmm");
     }
+
+    exe.linkSystemLibrary("opengl32");
+    exe.addLibraryPath(.{ .path = "deps/raylib/build/raylib/Release" });
+    exe.linkSystemLibrary("raylib");
 
     b.installArtifact(exe);
 
@@ -47,4 +79,8 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const raylib_step = buildRaylib(b);
+    const raylibCleanBuildStep = b.step("raylib", "Build raylib");
+    raylibCleanBuildStep.dependOn(raylib_step);
 }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include "raylib.h"
+
+const int screenWidth = 800;
+const int screenHeight = 600;
+
+
+void Draw() {
+    ClearBackground(RAYWHITE);
+    DrawText("Hello, Sailor!", 280, 200, 40, GRAY);
+}
 
 int main(void) {
-    fprintf(stderr, "Hello, Sailor!\n");
+
+    InitWindow(screenWidth, screenHeight, "Basic Window");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+
+        float deltaTime = GetFrameTime();
+        float deltaTimeMs = deltaTime * 1000.0f;
+
+        char *title = TextFormat("Basic Window - %.2f ms", deltaTimeMs);
+        SetWindowTitle(title);
+
+        BeginDrawing();
+            Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
